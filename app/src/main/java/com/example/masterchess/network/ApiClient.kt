0fee43a7e8cbb06f97af7f8e460e48c8215076a2
Biya Paul <bpsmartdesign@hotmail.com>
2025-07-29 20:32:02 +0100

    Update ApiClient.kt

--- app/src/main/java/com/example/masterchess/network/ApiClient.kt
+++ app/src/main/java/com/example/masterchess/network/ApiClient.kt
@@ -14,7 +14,9 @@ import io.ktor.serialization.kotlinx.json.*
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 
-private const val BASE_URL = "https://stockfish-server-2ik3.onrender.com"
+private const val BASE_URL = "http://172.20.10.6:3000"
+private const val BASE_URL_Emulator = "http://10.0.2.2:3000"
+private const val BASE_URL_ONLINE = "https://stockfish-server-2ik3.onrender.com"
 
 val client = HttpClient(OkHttp) {
     // Content negotiation for JSON
